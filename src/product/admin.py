4a3819760c5469cb226a575a09b6895471817834
khkoyani <khkoyani@gmail.com>
2019-08-05 16:47:01 -0400

    modified product model to have unique slug attributes

--- src/product/admin.py
+++ src/product/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+    # modifies product list in admin to display slug as well
+    list_display = ['__str__', 'slug']
+    class Meta:
+        model = Product
+
+admin.site.register(Product, ProductAdmin)
+
+
 # Register your models here.
