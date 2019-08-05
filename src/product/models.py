c15aecf735b7cad291321ccb04b50948e5283c2f
khkoyani <khkoyani@gmail.com>
2019-08-05 04:46:46 -0400

    updated model with an extended model manager
    updated view to use extended model manager to filter objects

--- src/product/models.py
+++ src/product/models.py
@@ -1,12 +1,39 @@
 from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
+import random
+import os
+
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+
+
+def upload_image_path(instance, filename):
+    print(instance)
+    print(filename)
+    new_filename = random.randint(1,35464123)
+    name, ext = get_filename_ext(filename)
+    final_filename = f'{new_filename}{ext}'
+    return f'product/{new_filename}/{final_filename}'
+
+class ProductManager(models.Manager):
+    '''extends model manager to add method to get by slug'''
+    def get_by_slug(self, slug):
+        qs = self.get_queryset().filter(slug=slug)
+        if qs.count() ==1:
+            return qs.first()
 
 class Product(models.Model):
     title = models.CharField(max_length=120, verbose_name='product_name', unique=True)
     description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    slug = models.SlugField(db_index=True, null=True, blank=True)
+    image = models.ImageField(upload_to=upload_image_path, blank=True, null=True)
+    slug = models.SlugField(db_index=True)
+    
+    objects = ProductManager()
 
     def __str__(self):
         return self.title
@@ -27,3 +54,4 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('product-detail', kwargs={'slug': self.slug})
+
--- src/product/views.py
+++ src/product/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Product
+from django.http import Http404
 
 
 
@@ -16,8 +17,19 @@ class ProductListView(ListView):
     #     return context
 #     used to find all the variables sent to template
 
+
 class ProductDetailView(DetailView):
     model = Product
+
+    def get_object(self,  *args, **kwargs ):
+        request = self.request
+        # gets the current request
+        slug = self.kwargs.get('slug')
+        # all parameters sent as part of the url are stored in kwargs
+        instance = Product.objects.get_by_slug(slug=slug)
+        if instance is None:
+            raise Http404("Product doesn't exist")
+        return instance
 
 class ProductCreateView(CreateView):
     pass
