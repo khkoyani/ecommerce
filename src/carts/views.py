504d34fcd4b32a8680bfb4684639bd341ced0797
khkoyani <khkoyani@gmail.com>
2019-08-08 03:16:28 -0400

    added toggling functionality to cart update

--- src/carts/views.py
+++ src/carts/views.py
@@ -4,12 +4,16 @@ from product.models import Product
 
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.get_or_new(request)
-    cart_obj.save()
-    return render(request, 'carts/home.html', {})
+    # cart_obj.save()
+    context = {'items': cart_obj.products.all()}
+    return render(request, 'carts/home.html', context)
 
 def cart_update(request):
-    product_id = 1
-    obj = Product.objects.get(id=product_id)
+    prod_id = 1
+    obj = Product.objects.get(id=prod_id)
     cart_obj, new_obj = Cart.objects.get_or_new(request)
-    cart_obj.products.add(obj)
-    return redirect(obj.get_absolute_url())
+    if obj in cart_obj.products.all():
+        cart_obj.products.remove(obj)
+    else:
+        cart_obj.products.add(obj)
+    return redirect(to='cart:home')
--- src/product/views.py
+++ src/product/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Product
+from carts.models import Cart
 from django.http import Http404
 
 
@@ -19,6 +20,12 @@ class ProductListView(ListView):
 
 class ProductDetailView(DetailView):
     model = Product
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data( *args, **kwargs)
+        cart_obj, new_obj = Cart.objects.get_or_new(self.request)
+        context['cart'] = cart_obj
+        return context
 
     def get_object(self, *args, **kwargs):
         slug = self.kwargs.get('slug')
