2082339ba6d6a8ff1ceb935457ad1d5529dc031a
khkoyani <khkoyani@gmail.com>
2019-08-10 16:53:14 -0400

    checkout finished
    project skeleton finished
    
    Remaining: styling, jquery, maybe react or angular

--- src/addresses/models.py
+++ src/addresses/models.py
@@ -17,3 +17,12 @@ class Address(models.Model):
 
     def __str__(self):
         return f"{str(self.billing_profile)}'s address"
+
+    def line_2(self):
+        if self.address_line_2:
+            return f'{self.address_line_2}\n'
+        else:
+            return ''
+    def get_address(self):
+
+        return f'{self.address_line_1},\n{self.line_2()}{self.city}, {self.state}, {self.zip_code}\n{self.country}'
